4b29a27e462f774570dc9906c40493b300c59bc6
Jill Allan <54241079+jillallan@users.noreply.github.com>
2023-06-30 15:59:44 +0100

    Add Trip sample data, and preview container

---
+++ Longitude/Preview Content/SampleData.swift
@@ -0,0 +1,16 @@
+//
+//  SampleData.swift
+//  Longitude
+//
+//  Created by Jill Allan on 30/06/2023.
+//
+
+import Foundation
+import SwiftData
+
+struct SampleData {
+    @MainActor
+    static func createData(modelContext: ModelContext) {
+        let trips = Trip.createSampleData(modelContext: modelContext)
+    }
+}
