4217896a3c5cb450d32e138f428c7c8b3976af83
Jill Allan <54241079+jillallan@users.noreply.github.com>
2023-06-30 10:35:46 +0100

    Initial Commit

---
+++ Longitude/LongitudeApp.swift
@@ -0,0 +1,20 @@
+//
+//  LongitudeApp.swift
+//  Longitude
+//
+//  Created by Jill Allan on 30/06/2023.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct LongitudeApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(for: Item.self)
+    }
+}
